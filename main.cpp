11fcc98cb047e5af27255d2e782f0f869d875d9d
CristobalHunto <76532078+CristobalHunto@users.noreply.github.com>
2025-04-13 14:10:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,32 +6,15 @@
 
 using namespace std;
 
-int main() {
-    int n;
-
-    cout << "Введите размер массива: ";
-    cin >> n;
-
-    if (n <= 0) {
-        cout << "Размер массива должен быть положительным." << endl;
-        return 1;
-    }
-
-    vector<double> arr(n);
-    cout << "Введите элементы массива: ";
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
-    }
-
-    //Используем алгоритмы из <algorithm> для нахождения min_element и max_element.
+template <typename Type>
+Type min_max_sum(vector<Type> arr)
+{
+    // Используем алгоритмы из <algorithm> для нахождения min_element и max_element.
     auto min_it = min_element(arr.begin(), arr.end());
     auto max_it = max_element(arr.begin(), arr.end());
 
-
-    if (min_it == arr.end() || max_it == arr.end()) {
-        cout << "Массив пуст." << endl;
-        return 1;
-    }
+    if (min_it == arr.end() || max_it == arr.end())
+        throw "Vecrot is empty!";
 
     double sum = 0; // используем long double для предотвращения переполнения с плавающей точкой
     int minIndex = distance(arr.begin(), min_it);
@@ -39,15 +22,63 @@ int main() {
     int start = min(minIndex, maxIndex);
     int end = max(minIndex, maxIndex);
 
-    
-
-    for (int i = start + 1; i < end; ++i) {
-        if (arr[i] < 0) {
+    for (int i = start + 1; i < end; ++i)
+    {
+        if (arr[i] < 0)
+        {
             sum += arr[i];
         }
     }
 
-    cout << "Сумма отрицательных элементов между максимальным и минимальным: " << sum << endl;
+    return sum;
+}
+
+int main()
+{
+    int n;
+    vector<double> arr;
+
+    // test
+    if (min_max_sum(vector<double>({0., -2., -1., -1., 3., 8.})) != -2.)
+    {
+        cout << "Test double is invalid." << endl;
+        return 1;
+    }
+
+    if (min_max_sum(vector<int>({-9, -8, -7, -6, -9})) != -15)
+    {
+        cout << "Test int is invalid." << endl;
+        return 1;
+    }
+
+    cout << "Input vector size: ";
+    cin >> n;
+
+    if (n <= 0)
+    {
+        cout << "Error size." << endl;
+        return 1;
+    }
+
+    arr.resize(n);
+    cout << "Input elepents of vector: ";
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+    }
+
+    double sum;
+    try
+    {
+        sum = min_max_sum(arr);
+    }
+    catch (const char *e)
+    {
+        std::cerr << e << '\n';
+        return 1;
+    }
+
+    cout << "Sum = : " << sum << endl;
 
     return 0;
 }
